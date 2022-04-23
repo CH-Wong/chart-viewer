f908182303846fdd6a000355b79e5856a0e9658e
CH-Wong <w.chunheung@gmail.com>
2022-04-24 00:40:03 +0200

    initialized react app

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPOk299hwHKRP4F6M5hFwyVtNVhQm-KP0",
+  authDomain: "bidoof-home-database.firebaseapp.com",
+  databaseURL: "https://bidoof-home-database-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bidoof-home-database",
+  storageBucket: "bidoof-home-database.appspot.com",
+  messagingSenderId: "774792325510",
+  appId: "1:774792325510:web:8bc61e5aebafe8b25ba407"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
