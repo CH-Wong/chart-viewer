e508fbb4330a8a3ac4ef65361a05ff2f2db75dee
CH-Wong <w.chunheung@gmail.com>
2022-04-24 21:43:23 +0200

    GOT DATA FROM FIREBASE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { firebaseConfig } from './firebase.js'
+import { initializeApp } from "firebase/app";
+
+
+
+import { getDatabase, ref, child, get } from "firebase/database";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const email = "w.chunheung@gmail.com"
+const password = "hellohellotesttest"
+
+
+var newdata = [];
+
+initializeApp(firebaseConfig)
+
+const auth = getAuth();
+const dbRef = ref(getDatabase());
+
+async function getData() {
+  var user;
+
+  await signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+  await get(child(dbRef, `livingroom/temperature`), user).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val())
+
+      for (let key in snapshot.val()) {
+        newdata.push(snapshot.val()[key])
+      };
+
+      console.log(newdata)
+
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+
+}
+
+getData();
+
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <canvas id="myChart"></canvas>
       </header>
     </div>
   );
 }
 
+
+// NEXT -> https://github.com/recharts/recharts
+// https://reactjs.org/docs/thinking-in-react.html
 export default App;
+
+
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDPOk299hwHKRP4F6M5hFwyVtNVhQm-KP0",
   authDomain: "bidoof-home-database.firebaseapp.com",
   databaseURL: "https://bidoof-home-database-default-rtdb.europe-west1.firebasedatabase.app",
@@ -13,6 +15,3 @@ const firebaseConfig = {
   messagingSenderId: "774792325510",
   appId: "1:774792325510:web:8bc61e5aebafe8b25ba407"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
