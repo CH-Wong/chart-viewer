8074f1de8c7e1a5dbf025dcecde32abe1472c01b
CH-Wong <w.chunheung@gmail.com>
2022-04-29 01:07:16 +0200

    i dont like javascript

--- src/App.js
+++ src/App.js
@@ -4,44 +4,27 @@ import './App.css';
 import { firebaseConfig } from './firebase.js'
 import { initializeApp } from "firebase/app";
 
-
-
 import { getDatabase, ref, child, get } from "firebase/database";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+import * as d3 from "d3";
 
 const email = "w.chunheung@gmail.com"
 const password = "hellohellotesttest"
 
-
-var newdata = [];
+const data = [];
 
 initializeApp(firebaseConfig)
 
-const auth = getAuth();
-const dbRef = ref(getDatabase());
+function getData(user, dBRef, address, outputArray) {
 
-async function getData() {
-  var user;
-
-  await signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    var user = userCredential.user;
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-
-  await get(child(dbRef, `livingroom/temperature`), user).then((snapshot) => {
+  get(child(dBRef, address), user).then((snapshot) => {
     if (snapshot.exists()) {
       console.log(snapshot.val())
 
       for (let key in snapshot.val()) {
-        newdata.push(snapshot.val()[key])
+        data.push(snapshot.val()[key])
       };
-
-      console.log(newdata)
 
     } else {
       console.log("No data available");
@@ -50,17 +33,135 @@ async function getData() {
     console.error(error);
   });
 
+  return outputArray;
+};
+
+async function formatData() {
+  const auth = getAuth();
+  const dbRef = ref(getDatabase());
+
+  await signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    var temperature = [];
+    var humidity = [];
+    var time = [];
+  
+    getData(user, dbRef, 'workyroom/temperature', temperature);
+    getData(user, dbRef, 'workyroom/humidity', humidity);
+    getData(user, dbRef, 'workyroom/time', time);
+  
+    time.forEach(function(element, index) {
+      data.push({"time": element*1000, "temperature": temperature[index], "humidity": humidity[index]})
+
+    })
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+
 }
 
-getData();
+// Copyright 2021 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/line-chart
+function LineChart(data, {
+  x = ([x]) => x, // given d in data, returns the (temporal) x-value
+  y = ([, y]) => y, // given d in data, returns the (quantitative) y-value
+  defined, // for gaps in data
+  curve = d3.curveLinear, // method of interpolation between points
+  marginTop = 20, // top margin, in pixels
+  marginRight = 30, // right margin, in pixels
+  marginBottom = 30, // bottom margin, in pixels
+  marginLeft = 40, // left margin, in pixels
+  width = 640, // outer width, in pixels
+  height = 400, // outer height, in pixels
+  xType = d3.scaleUtc, // the x-scale type
+  xDomain, // [xmin, xmax]
+  xRange = [marginLeft, width - marginRight], // [left, right]
+  yType = d3.scaleLinear, // the y-scale type
+  yDomain, // [ymin, ymax]
+  yRange = [height - marginBottom, marginTop], // [bottom, top]
+  yFormat, // a format specifier string for the y-axis
+  yLabel, // a label for the y-axis
+  color = "currentColor", // stroke color of line
+  strokeLinecap = "round", // stroke line cap of the line
+  strokeLinejoin = "round", // stroke line join of the line
+  strokeWidth = 1.5, // stroke width of line, in pixels
+  strokeOpacity = 1, // stroke opacity of line
+} = {}) {
+  // Compute values.
+  const X = d3.map(data, x);
+  const Y = d3.map(data, y);
+  const I = d3.range(X.length);
+  if (defined === undefined) defined = (d, i) => !isNaN(X[i]) && !isNaN(Y[i]);
+  const D = d3.map(data, defined);
+
+  // Compute default domains.
+  if (xDomain === undefined) xDomain = d3.extent(X);
+  if (yDomain === undefined) yDomain = [0, d3.max(Y)];
+
+  // Construct scales and axes.
+  const xScale = xType(xDomain, xRange);
+  const yScale = yType(yDomain, yRange);
+  const xAxis = d3.axisBottom(xScale).ticks(width / 80).tickSizeOuter(0);
+  const yAxis = d3.axisLeft(yScale).ticks(height / 40, yFormat);
+
+  // Construct a line generator.
+  const line = d3.line()
+      .defined(i => D[i])
+      .curve(curve)
+      .x(i => xScale(X[i]))
+      .y(i => yScale(Y[i]));
+
+  const svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height])
+      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+
+  svg.append("g")
+      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("transform", `translate(${marginLeft},0)`)
+      .call(yAxis)
+      .call(g => g.select(".domain").remove())
+      .call(g => g.selectAll(".tick line").clone()
+          .attr("x2", width - marginLeft - marginRight)
+          .attr("stroke-opacity", 0.1))
+      .call(g => g.append("text")
+          .attr("x", -marginLeft)
+          .attr("y", 10)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "start")
+          .text(yLabel));
+
+  svg.append("path")
+      .attr("fill", "none")
+      .attr("stroke", color)
+      .attr("stroke-width", strokeWidth)
+      .attr("stroke-linecap", strokeLinecap)
+      .attr("stroke-linejoin", strokeLinejoin)
+      .attr("stroke-opacity", strokeOpacity)
+      .attr("d", line(I));
+
+  return svg.node();
+}
+
 
 
 function App() {
+  formatData();
 
   return (
     <div className="App">
       <header className="App-header">
-      <canvas id="myChart"></canvas>
+        <LineChart data={data} />
       </header>
     </div>
   );
